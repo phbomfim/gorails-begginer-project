3d4047a2e1b8b4b1264b7520d7db125cf17b9b3a
phbomfim <paulobommfim@gmail.com>
2021-03-25 12:20:58 -0300

    Define a keys and method to authenticate tweets

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -3,4 +3,13 @@ class TwitterAccount < ApplicationRecord
   has_many :tweets
 
   validates :username, uniqueness: true #only accept a unique twitter_account.username each user
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.credentials.dig(:twitter, :api_key) #From our API
+      config.consumer_secret = Rails.application.credentials.dig(:twitter, :api_secret) #In /config/initalizers/omniauth.rb
+      config.access_token = token #From our database
+      config.access_token_secret = secret
+    end
+  end
 end
